8ff22cab13834c91b106d781e77af8f492d4d247
Bastian Prado <bastian.prado.v@gmail.com>
2026-04-13 00:49:53 -0700

    Initial commit

---
+++ src/main/java/com/pmdspringbootrestapitemplate/PmdSpringbootRestApiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.pmdspringbootrestapitemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmdSpringbootRestApiTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmdSpringbootRestApiTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pmdspringbootrestapitemplate/PmdSpringbootRestApiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmdspringbootrestapitemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmdSpringbootRestApiTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
